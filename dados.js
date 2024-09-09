dabec0784520b446cdb6364fcc3e518003310fca
GeisilaCosta <geisilascosta@gmail.com>
2024-09-09 16:18:09 -0300

    add dados

--- dados.js
+++ dados.js
@@ -12,6 +12,18 @@ let dados = [
         tags: "judô judo judoca judoka"
     },
     {
+        titulo: "Adhemar Ferreira da Silva",
+        descricao: "Adhemar Ferreira da Silva foi o primeiro brasileiro a conquistar duas medalhas de ouro olímpicas consecutivas, no salto triplo. Sua marca permaneceu invicta por quase 50 anos.",
+        link: "https://pt.wikipedia.org/wiki/Adhemar_Ferreira_da_Silva",
+        tags: "atletismo salto triplo ouro olimpico"
+    },
+    {
+        titulo: "Marta Vieira da Silva",
+        descricao: "Marta é considerada a maior jogadora de futebol feminino de todos os tempos. A brasileira acumula diversos títulos e artilharias em sua carreira.",
+        link: "https://pt.wikipedia.org/wiki/Marta_Vieira_da_Silva",
+        tags: "futebol feminino bola de ouro"
+    },
+    {
         titulo: "Rebeca Andrade",
         descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Aos poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.",
         link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
